888dd8992d97a8f7e9614fcf2267575036642fe9
tanyasharma02 <143033981+tanyasharma02@users.noreply.github.com>
2025-04-30 20:02:00 -0400

    Update upload.sh

--- labs/lab4/upload.sh
+++ labs/lab4/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo " $0 <file> <bucket>"
     exit 1
 fi
@@ -8,11 +8,12 @@ fi
 #parameters
 FILE=$1
 BUCKET=$2
+EXPIRATION=$3
 
 #uploading file
 aws s3 cp "$FILE" "s3://$BUCKET/"
 
 #making link
-URL=$(aws s3 presign --expires-in 604800 "s3://$BUCKET/$FILE")
+URL=$(aws s3 presign "s3://$BUCKET/$(basename "$FILE")" --expires-in "$EXPIRATION")
 
 echo "URL: $URL"
